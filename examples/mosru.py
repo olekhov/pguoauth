cbff0aa2f9cb9abe3542a92d31c4c6f97b90e70c
Vasiliy Olekhov <olekhov@gmail.com>
2019-06-02 18:34:40 +0300

    Работает аутентификация с esia

--- examples/mosru.py
+++ examples/mosru.py
@@ -10,8 +10,88 @@ sys.path.insert(0, os.path.join(os.path.dirname( __file__ ), "..", "pguoauth"))
 from pguoauth import PGUAuthenticator
 
 import json
+import logging
+import requests
+import re
+import pdb
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("chardet").setLevel(logging.WARNING)
 
 with open('config.json') as json_data_file:
-    data = json.load(json_data_file)
-    print(data)
+    pguconfig = json.load(json_data_file)
+
+au = PGUAuthenticator(pguconfig)
+ps=requests.Session()
+popular="https://www.mos.ru/services/catalog/popular/"
+
+logging.debug("Открываем портал www.mos.ru")
+r_ae=ps.get("https://login.mos.ru/sps/oauth/ae?client_id=Wiu8G6vfDssAMOeyzf76&response_type=code&redirect_uri=https://my.mos.ru/my/website_redirect_uri&scope=openid+profile", allow_redirects=False)
+if r_ae.status_code != 303 or r_ae.headers['Location']!="/sps/login/methods/password":
+    logging.error("Церемония поменялась")
+    raise
+#ps.cookies.update(r.cookies)
+r_password=ps.get("https://login.mos.ru"+r_ae.headers['Location'], allow_redirects=False)
+logging.debug("Начало аутентификационной сессии")
+r_opts=ps.get("https://www.mos.ru/api/oauth20/v1/frontend/json/ru/options", headers={"referer":popular})
+logging.debug("Вход")
+r_enter=ps.get(f"https://www.mos.ru/api/oauth20/v1/frontend/json/ru/process/enter?redirect={popular}",
+        cookies=r_opts.cookies, allow_redirects=False)
+
+if r_enter.status_code !=302:
+    logging.error("Церемония поменялась")
+    raise
+r_authorize=ps.get(r_enter.headers['Location'], allow_redirects=False)
+logging.debug("Переход на форму авторизации")
+if r_enter.status_code !=302:
+    logging.error("Церемония поменялась")
+    raise
+
+#ps.cookies.update(r_password.cookies)
+#ps.cookies.update(r.cookies)
+r_ae2=ps.get(r_authorize.headers['Location'], allow_redirects=False)
+
+if r_ae2.status_code !=303 or r_ae2.headers['Location']!="/sps/login/methods/password":
+    logging.error("Церемония поменялась")
+    raise
+
+r_password2=ps.get("https://login.mos.ru"+r_ae2.headers['Location'], allow_redirects=False, cookies=r_ae2.cookies)
+if r_password2.status_code != 200 :
+    logging.error("Церемония поменялась")
+    raise
+
+logging.debug("Выбираем вариант входа: через госуслуги")
+
+r_execute=ps.get("https://login.mos.ru/sps/login/externalIdps/execute?typ=esia&name=esia_1&isPopup=false",
+        headers={"referer": "https://login.mos.ru/sps/login/methods/password"}, 
+        cookies=r_ae2.cookies, allow_redirects=False)
+
+if r_execute.status_code !=303 :
+    logging.error("Церемония поменялась")
+    raise
+
+esia_request=r_execute.headers["Location"]
+code=au.AuthenticateByEmail(esia_request, "https://login.mos.ru")
+
+# в code должен быть хороший ответ типа 
+# https://login.mos.ru/sps/login/externalIdps/callback/esia/esia_1/false?c
+# ode=eyJ2ZXIiOjEsInR5cCI6IkpXVCIsInNidCI6ImF1dGhvcml6YXRpb25fY29...
+
+callback_cookies={
+        'fm': r_ae.cookies['fm'],
+        'history': r_execute.cookies['history'],
+        'lstate' : r_execute.cookies['lstate'],
+        'oauth_az':r_ae.cookies['oauth_az'],
+        'origin': r_ae.cookies['origin']}
+
+
+r_callback = ps.get(code, allow_redirects=False, 
+        cookies=callback_cookies,
+        headers={'referer':'https://esia.gosuslugi.ru/'})
+if r_callback.cookies['Ltpatoken2'] != '' :
+    print("Авторизовано успешно")
+
+
--- pguoauth/pguoauth.py
+++ pguoauth/pguoauth.py
@@ -10,13 +10,152 @@ class PGUAuthenticator:
     def __init__(self, cfg):
         self._ps = requests.Session()
         self._cfg = cfg
-        self._ps.headers['User-Agent'] = self._cfg.UA
+        self._ps.headers['User-Agent'] = self._cfg["UserAgent"]
         self.Ltpatoken2 = ""
         self.Authenticated = False
         pass
     
 
-    def Authenticate(self):
-        return self.Authenticated
+    """
+    Аутентификация.
+    Входные параметры: 
+    URL - редирект-урл от подсистемы.
+    вид: https://esia.gosuslugi.ru/aas/oauth2/ac? client_id=xx & client_secret= xx ..
+    & response_type=code
+
+    req - куки, полученные от системы, запрашивающей аутентификацию
+    Примерные значения:
+    _idp_authn_lc_key: 71229dcf-0202-483a-8621-b02098dfec05
+    idp_id: ddf2a27da6eaf5e15c3bd0c57f23097d
+    oauth_id: xxx
+    SCS: rg5xBKgH02x...
+    JSESSIONID: B9A78A899ED04EA067BE01C997370097
+    RelayState: guid
+    command: base64.base64.base64
+    На вход необходимо передать словарь с этими данными 
+    """
+    def AuthenticateByEmail(self,url,referer):
+        r_ac = self._ps.get(url,allow_redirects=False, 
+                headers={"referer":referer})
+        meta_redir = re.search('<meta http-equiv="refresh" content="0;url=([^"]*)">',r_ac.text) 
+        r_SSO = self._ps.get(meta_redir.group(1), cookies=r_ac.cookies, allow_redirects=False)
+        if r_SSO.status_code != 302:
+            logging.error("Церемония поменялась")
+            raise
+
+        AuthnEngine_cookies={
+                '_idp_authn_lc_key' : r_SSO.cookies['_idp_authn_lc_key'],
+                'idp_id':r_SSO.cookies['idp_id'],
+                'JSESSIONID':r_ac.cookies['JSESSIONID'] }
+
+        r_AuthnEngine=self._ps.get(r_SSO.headers["Location"], allow_redirects=False,
+                cookies=AuthnEngine_cookies)
+
+        command=re.search("LoginViewModel\('/idp','','(.*)','','null','null',false, 300, 'gosuslugi.ru'\);",
+                r_AuthnEngine.text )
+
+
+        #pdb.set_trace()
+        login_data={
+                "mobileOrEmail":self._cfg["login"],
+                "snils":"",
+                "password":self._cfg["password"],
+                "login":self._cfg["login"],
+                "command":command.group(1),
+                "idType":"email" }
+
+        pwd_cookies={
+                '_idp_authn_id': 'email:'+self._cfg["login"],
+                '_idp_authn_lc_key':r_SSO.cookies['_idp_authn_lc_key'],
+                'idp_id':r_AuthnEngine.cookies['idp_id'],
+                'SCS':r_AuthnEngine.cookies['SCS'],
+                'JSESSIONID':r_ac.cookies['JSESSIONID'],
+                'login_value':self._cfg["login"],
+                'oiosaml-fragment':'',
+                'timezone':'3',
+                'userSelectedLanguage':'ru'}
+
+        #pdb.set_trace()
+        r_pwddo=self._ps.post("https://esia.gosuslugi.ru/idp/login/pwd/do", 
+                data=login_data, 
+                headers={"referer":"https://esia.gosuslugi.ru/idp/rlogin?cc=bp"},
+                cookies=pwd_cookies,
+                allow_redirects=False)
+        if r_pwddo.status_code !=302 :
+            print("Церемония поменялась")
+
+        SSO2_cookies={
+                '_idp_authn_id': 'email:'+self._cfg["login"],
+                '_idp_authn_lc_key':r_SSO.cookies['_idp_authn_lc_key'],
+                '_idp_session':r_pwddo.cookies['_idp_session'],
+                'idp_id':r_AuthnEngine.cookies['idp_id'],
+                'SCS':r_AuthnEngine.cookies['SCS'],
+                'JSESSIONID':r_ac.cookies['JSESSIONID'],
+                'login_value':self._cfg["login"],
+                'oiosaml-fragment':'',
+                'timezone':'3',
+                'userSelectedLanguage':'ru'}
+
+        r_SSO2=self._ps.get(r_pwddo.headers['Location'],
+                allow_redirects=False,
+                headers={"referer":"https://esia.gosuslugi.ru/idp/rlogin?cc=bp"},
+                cookies=SSO2_cookies)
+
+        samlr=re.search('<input type="hidden" name="SAMLResponse" value="(.*)"/>',r_SSO2.text)
+
+        SAMLResponse=samlr.group(1)
+
+        post_data={
+                'RelayState':re.search('RelayState=([-_a-z0-9]*)',meta_redir.group(1)).group(1),
+                'SAMLResponse':SAMLResponse}
+        consumer_cookies={
+                '_idp_authn_id': 'email:'+self._cfg["login"],
+                'bs':r_pwddo.cookies['bs'],
+                'idp_id':r_AuthnEngine.cookies['idp_id'],
+                'SCS':r_AuthnEngine.cookies['SCS'],
+                'JSESSIONID':r_ac.cookies['JSESSIONID'],
+                'login_value':self._cfg["login"],
+                'oauth_id':r_ac.cookies['oauth_id'],
+                'oiosaml-fragment':'',
+                'timezone':'3',
+                'userSelectedLanguage':'ru'}
+
+        r_SAMLAC=self._ps.post("https://esia.gosuslugi.ru/aas/oauth2/saml/SAMLAssertionConsumer",
+                data=post_data,
+                allow_redirects=False,
+                headers={'referer':'https://esia.gosuslugi.ru/idp/profile/SAML2/Redirect/SSO'},
+                cookies=consumer_cookies)
+
+        if r_SAMLAC.status_code !=302 :
+            logging.error("Церемония поменялась")
+            raise
+        #pdb.set_trace()
+
+        self._ps.cookies.clear()
+        r_acfinish=self._ps.get(r_SAMLAC.headers['location'],
+                allow_redirects=False,
+                headers={'referer':'https://esia.gosuslugi.ru/idp/profile/SAML2/Redirect/SSO'},
+                cookies=consumer_cookies)
+
+        if r_acfinish.status_code !=302 :
+            logging.error("Церемония поменялась")
+            raise
+
+        #pdb.set_trace()
+        #callback_cookies={
+        #        'fm': r_ae.cookies['fm'],
+        #        'history': r_execute.cookies['history'],
+        #        'lstate' : r_execute.cookies['lstate'],
+        #        'oauth_az':r_ae.cookies['oauth_az'],
+        #        'origin': r_ae.cookies['origin']}
+
+        #r_callback=ps.get(r_acfinish.headers['location'],allow_redirects=False,
+        #        cookies=callback_cookies,
+        #        headers={'referer':'https://esia.gosuslugi.ru/'})
+
+        #self.Ltpatoken2 = r_callback.cookies['Ltpatoken2']
+        #self.Authenticated = self.Ltpatoken2 != ""
+
+        return r_acfinish.headers['location']
 
         
